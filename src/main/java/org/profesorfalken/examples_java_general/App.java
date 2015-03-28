813616f006d5196be13a8ed373a0c09f0c7f770c
javier <jgarciaalo@gmail.com>
2015-03-28 23:43:41 +0100

    Added default maven app elements

---
+++ src/main/java/org/profesorfalken/examples_java_general/App.java
@@ -0,0 +1,13 @@
+package org.profesorfalken.examples_java_general;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
